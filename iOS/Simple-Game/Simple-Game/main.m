d69dcbdf91bb641e8947f31fcc345c194d0bf6e4
Max Tymchii <samback@ukr.net>
2012-12-16 17:48:14 +0200

    Initial commit

---
+++ iOS/Simple-Game/Simple-Game/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Simple-Game
+//
+//  Created by Max on 16.12.12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
